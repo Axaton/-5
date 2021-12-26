60d6b506cbc30b5613930f8414131b4d5bf342b2
darkd6002@outlook.com <darkd6002@outlook.com>
2021-12-26 14:26:25 +0300

    Добавьте файлы проекта.

---
+++ Практическая №5/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практическая__5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void IncreaseBy1_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(firstTriad1.Text) > 0 || int.Parse(secondTriad1.Text) > 0 || int.Parse(thirdTriad1.Text) > 0)
+            {
+                MessageBox.Show("Значения должны быть отрицательными");
+            }
+            else
+            {
+                Triad firstTriad = new Triad(int.Parse(firstTriad1.Text), int.Parse(secondTriad1.Text), int.Parse(thirdTriad1.Text));
+                Triad result = firstTriad.Add();
+                firstTriad1.Text = result.First.ToString();
+                secondTriad1.Text = result.Second.ToString();
+                thirdTriad1.Text = result.Third.ToString();
+            }
+        }
+
+        private void IncreaseBy10_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(firstTriad1.Text) > 0 || int.Parse(secondTriad1.Text) > 0 || int.Parse(thirdTriad1.Text) > 0)
+            {
+                MessageBox.Show("Значения должны быть отрицательными");
+            }
+            else
+            {
+                Triad firstTriad = new Triad(int.Parse(firstTriad1.Text), int.Parse(secondTriad1.Text), int.Parse(thirdTriad1.Text));
+                Triad result = firstTriad.SetParams();
+                firstTriad1.Text = result.First.ToString();
+                secondTriad1.Text = result.Second.ToString();
+                thirdTriad1.Text = result.Third.ToString();
+            }
+        }
+
+        private void SetParams_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(first.Text) > 0 || int.Parse(second.Text) > 0 || int.Parse(third.Text) > 0)
+            {
+                MessageBox.Show("Значения должны быть отрицательными");
+            }
+            else
+            {
+                Triad firstTriad = new Triad(int.Parse(firstTriad1.Text), int.Parse(secondTriad1.Text), int.Parse(thirdTriad1.Text));
+                Triad result = firstTriad.SetParams(int.Parse(first.Text), int.Parse(second.Text), int.Parse(third.Text));
+                firstTriad1.Text = result.First.ToString();
+                secondTriad1.Text = result.Second.ToString();
+                thirdTriad1.Text = result.Third.ToString();
+            }
+        }
+
+      
+        private void TaskInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Создать класс Triad (тройка отрицательных чисел). Создать необходимые методы и " +
+            "свойства.Определить метод увеличения полей на 1.Создать перегруженные методы SetParams, " +
+               "для установки параметров объекта, в том числе увеличения всех чисел на 10.", "Задание");
+        }
+
+
+        private void DeveloperInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Харенко Кирилл  ИСП-34", "Разработчик", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Практическая №5/Triad.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая__5
+{
+    class Triad
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Third { get; set; }
+
+        public Triad(int first, int second, int third)
+        {
+            First = first;
+            Second = second;
+            Third = third;
+        }
+
+        public Triad Add(int increase = 1)
+        {
+            return new Triad(First + increase, Second + increase, Third + increase);
+        }
+
+        public Triad SetParams(int increase = 10)
+        {
+            return new Triad(First + increase, Second + increase, Third + increase);
+        }
+
+        public Triad SetParams(int first, int second, int third)
+        {
+            return new Triad(First = first, Second = second, Third = third);
+        }
+    }
+}
